4dc855fbeb2a6654234a2370963dc4a0f4e4964e
lurdharry <azeezhammed57@gmail.com>
2025-02-04 12:24:08 +0100

    add school utility methods

---
+++ src/main/java/com/lurdharry/springDemo/School/SchoolMapper.java
@@ -0,0 +1,15 @@
+package com.lurdharry.springDemo.School;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SchoolMapper {
+
+     School  toSchool(SchoolDto schoolDto) {
+        return  new School(schoolDto.name());
+    }
+
+     SchoolDto toSchoolDto (School school){
+        return  new SchoolDto(school.getName());
+    }
+}

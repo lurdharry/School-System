530d12db877b2d3cf81b9f1da5f8dbfedb2f91c5
lurdharry <azeezhammed57@gmail.com>
2025-02-04 12:24:54 +0100

    add services logic

---
+++ src/main/java/com/lurdharry/springDemo/School/SchoolService.java
@@ -0,0 +1,32 @@
+package com.lurdharry.springDemo.School;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SchoolService {
+    private final SchoolRepository schoolRepository;
+    private final SchoolMapper schoolMapper;
+
+    public SchoolService(SchoolRepository schoolRepository, SchoolMapper schoolMapper) {
+        this.schoolRepository = schoolRepository;
+        this.schoolMapper = schoolMapper;
+    }
+
+    SchoolDto create(
+            SchoolDto schoolDto
+    ){
+        var school = schoolMapper.toSchool(schoolDto);
+        schoolRepository.save(school);
+        return schoolDto;
+    }
+
+    List<SchoolDto> findAll (){
+        return schoolRepository.findAll()
+                .stream()
+                .map(schoolMapper::toSchoolDto)
+                .collect(Collectors.toList());
+    }
+}

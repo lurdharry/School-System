5a31cf4ce3a38db6616ff3d9c6e3407ed5eb91a3
lurdharry <azeezhammed57@gmail.com>
2025-02-04 12:24:27 +0100

    add repositories

---
+++ src/main/java/com/lurdharry/springDemo/Student/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.lurdharry.springDemo.Student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface StudentRepository extends JpaRepository<Student,Integer> {
+
+     List<Student> findAllByFirstNameContaining(String firstName);
+}
